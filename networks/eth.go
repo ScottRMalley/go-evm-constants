bb23e1219a3d5a8fabadb3e69a4c69cefa2c7fee
Scott R. Malley <scott.r.malley@gmail.com>
2022-09-13 16:44:20 +0200

    updated ethereum networks

--- networks/eth.go
+++ networks/eth.go
@@ -6,7 +6,7 @@ var ETHEREUM_MAINNET Name = "eth"
 
 var ethereum = Network{
 	Name:    ETHEREUM_MAINNET,
-	RpcUrl:  "https://mainnet.infura.io/v3/",
+	RpcUrl:  "https://rpc.ankr.com/eth/",
 	ChainId: big.NewInt(1),
 	Testnet: false,
 }
@@ -15,7 +15,7 @@ var ETHEREUM_GOERLI Name = "eth-goerli"
 
 var ethereumGoerli = Network{
 	Name:    ETHEREUM_GOERLI,
-	RpcUrl:  "https://goerli.infura.io/v3/",
+	RpcUrl:  "https://ethereum-goerli-rpc.allthatnode.com/",
 	ChainId: big.NewInt(5),
 	Testnet: true,
 }
